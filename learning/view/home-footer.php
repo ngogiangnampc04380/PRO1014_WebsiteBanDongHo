d6572c72e6a52829e8ebc35ee847a6e31c423330
JackKy17471 <148345405+JackKy17471@users.noreply.github.com>
2024-03-09 18:35:49 +0700

    [HTML]-Home,product,footer

---
+++ learning/view/home-footer.php
@@ -0,0 +1,39 @@
+<div class="container" st>
+            <div class="row">
+            <div class="col-12 col-md-6 col-gl-4 col-xl-3">
+                <ul class="menu-footer">
+                    <li><a href="">Tích điểm Quà tặng VIP</a></li>
+                    <li><a href="">Lịch sử mua hàng</a></li>
+                    <li><a href="">Tìm hiểu về mua trả góp</a></li>
+                    <li><a href="">Chính sách bảo hành</a></li>
+                    <li><a href="">Chính sách đổi trả</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 col-gl-4 col-xl-3">
+            <ul class="menu-footer">
+                    <li><a href="">Giới thiệu công ty (MWG.vn)</a></li>
+                    <li><a href="">Tuyển dụng</a></li>
+                    <li><a href="">Gửi góp ý, khiếu nại</a></li>
+                    <li><a href="">Tìm siêu thị (3.167 shop)</a></li>
+                    <li><a href="">Xem bản mobile</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 col-gl-4 col-xl-3">
+            <ul class="menu-footer">
+
+                    <li><a href="">Gọi mua: 1900 232 460 (7:30 - 22:00)</a></li>
+                    <li><a href="">Khiếu nại: 1800.1062 (8:00 - 21:30)</a></li>
+                    <li><a href="">Bảo hành: 1900 232 464 (8:00 - 21:00)</a></li>
+                    
+                </ul>
+            </div>
+            
+            <div class="col-12 col-md-6 col-gl-4 col-xl-3">
+            <ul class="menu-footer">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31434.252401151974!2d105.74354061357788!3d9.993585898098814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a08906415c355f%3A0x416815a99ebd841e!2zVHLGsOG7nW5nIENhbyDEkeG6s25nIEZQVCBQb2x5dGVjaG5pYw!5e0!3m2!1svi!2s!4v1709816295572!5m2!1svi!2s" width="300" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    
+                </ul>
+            </div>
+            </div>
+            
+        </div>
---
+++ learning/view/home-product.php
@@ -0,0 +1,24 @@
+<div class="container mt-2">
+          <div class="section-product py-5">
+            <div class="titel-section"><h4>San pham noi bat</h4></div>
+          <div class="row">
+                <?php for ($i = 0; $i < 8; $i++) : ?>
+                    <div class="col-12 col-md-6 col-gl-4 col-xl-3">
+                        <div class="card mb-4">
+                            <img src="/img/hinh dong ho 1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                         
+                    </div>
+                    <?php endfor; ?>
+
+               
+                </div>
+          </div> 
+                
+
+        </div>
